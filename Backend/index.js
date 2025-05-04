f735fcc31ae5dbe694088c4bccd989dcd4a126e7
nidakula <kakaachakaca@gmail>
2025-05-04 22:06:19 +0700

    cape

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL
+    origin: [process.env.FRONTEND_URL,
+    'http://localhost:5173']
   }));
 
 app.use('/notes', require('./src/routes/note.route'));
